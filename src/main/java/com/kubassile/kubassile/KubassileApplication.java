8e1c62f1db2dc3b881bb30ad36860d795660570d
AlexandreMahumane <alexandremahumane69@gmail.com>
2025-01-12 11:51:39 +0200

    project initializer

---
+++ src/main/java/com/kubassile/kubassile/KubassileApplication.java
@@ -0,0 +1,13 @@
+package com.kubassile.kubassile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubassileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubassileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubassile/kubassile/KubassileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubassile.kubassile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubassileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
